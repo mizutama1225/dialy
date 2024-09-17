fe02e754664d946251b20d9947d2850e23320152
wada <forkkst@gmail.com>
2024-09-17 23:22:11 +0900

    wadacommit

---
+++ frontend/lib/dialy_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:dialy/models/dialy.dart';
+import 'package:dialy/dialy_item.dart';
+
+class DialyList extends StatelessWidget {
+  DialyList({super.key, required this.dialies});
+
+  final List<Dialy> dialies;
+
+  @override
+  Widget build(context) {
+    return ListView.builder(
+        itemCount: dialies.length,
+        itemBuilder: (ctx, index) => Dismissible(
+            key: ValueKey(dialies[index]), child: DialyItem(dialies[index])));
+  }
+}
